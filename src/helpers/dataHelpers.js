b79eb268570f8f0779adb8b8e1cda6750f8093d7
Ernesto Gilberto Valenzuela Gallardo <ernestogilberto@hotmail.com>
2023-03-27 14:02:29 -0600

    adding comments

--- src/helpers/dataHelpers.js
+++ src/helpers/dataHelpers.js
@@ -1,8 +1,11 @@
+// Function to set ID of a product
 const setId = (data) => {
     try {
+        // If the status of the response is not 'success', set ID to 1
         if (data.status !== 'success') {
             return 1;
         } else {
+            // Get the list of products from the response and set ID to last product's ID + 1
             const products = data.payload;
             const id = products[products.length - 1].id;
             return id + 1;
@@ -12,12 +15,14 @@ const setId = (data) => {
     }
 };
 
+// Function to validate a product
 const validateProduct = (product, requiredProductFields, productsResult) =>{
-
+    // Check if the product exists, if not, return error
     if (!product) {
         return {status: 'failure', payload: 'Product is required'};
     }
 
+    // Check if all required fields of the product exist, if not, return error
     for (const field of requiredProductFields) {
         const {[field]: value} = product;
         if (!value) {
@@ -25,20 +30,24 @@ const validateProduct = (product, requiredProductFields, productsResult) =>{
         }
     }
 
+    // Check if any fields of the product are not allowed, if not, return error
     const allowedFieldsResult = validateAllowedFields(product, requiredProductFields);
     if (allowedFieldsResult.status === 'failure') {
         return {status: 'failure', payload: allowedFieldsResult.payload};
     }
 
     try {
+        // If there is an error getting the products, return error
         if (productsResult.status === 'failure') {
             return {status: 'failure', payload: 'Error getting products'};
         }
 
+        // If there are no products, return an empty array
         if (productsResult.status === '404') {
             return {status: 'success', payload: []};
         }
 
+        // Check if a product with the same code exists, if so, return error
         const products = productsResult.payload;
         const productFound = products.find(p => p.code === product.code);
 
@@ -46,12 +55,14 @@ const validateProduct = (product, requiredProductFields, productsResult) =>{
             return {status: 'failure', payload: `Product with code ${product.code} already exists`};
         }
 
-        return {status: 'success', payload: []};
+        // If everything is successful, return a status of 'success'
+        return {status: 'success'};
     } catch (error) {
         return {status: 'failure', payload: `Product validation error: ${error.message}`};
     }
 }
 
+// Function to validate if all fields of a product are allowed
 const validateAllowedFields = (data, requiredProductFields) => {
     const productKeys = Object.keys(data);
     for (const key of productKeys) {
@@ -59,6 +70,7 @@ const validateAllowedFields = (data, requiredProductFields) => {
             return {status: 'failure', payload: `Product property ${key} is not allowed`};
         }
     }
+    // If everything is successful, return a status of 'success'
     return {status: 'success'};
 }
 
--- src/managers/productManager.js
+++ src/managers/productManager.js
@@ -8,58 +8,77 @@ class ProductManager {
         this.path = path;
     }
 
+    // Method to get all products from the file
+    async getProducts() {
+        try {
+            // Reading the file containing the products
+            const products = await fs.readFile(this.path, 'utf8');
+
+            // Parsing the contents of the file and returning an object with a status of 'success' and the products
+            return {status: 'success', payload: JSON.parse(products)};
+
+        } catch (error) {
+            // If the file is not found, returning an object with a status of '404' and an empty array
+            if (error.code === 'ENOENT') {
+                return {status: '404', payload: []}
+            }
+            // If there is an error, returning an object with a status of 'failure' and the error message
+            return {status: 'failure', payload: error.message}
+        }
+    }
+
+    // Method to add a product to the file
     async addProduct(product) {
 
+        // Validating the product's fields using the validateProduct helper function
         const validationResult = validateProduct(product, this.requiredProductFields, await this.getProducts());
 
+        // If validation fails, returning an object with a status of 'failure' and the validation payload
         if (validationResult.status === 'failure') {
             return {status: 'failure', payload: validationResult.payload};
         }
 
+        // Setting the product's ID using the setId helper function
         product.id = await setId(await this.getProducts());
 
         try {
+            // Getting all products
             const productsResult = await this.getProducts();
 
+            // If there is an error, returning an object with a status of 'failure' and an error message
             if (productsResult.status === 'failure') {
                 return {status: 'failure', payload: 'Error getting products'};
             }
 
+            // Adding the new product to the products array and writing to the file
             const products = productsResult.payload;
-
             products.push(product);
             await fs.writeFile(this.path, JSON.stringify(products));
+
+            // Returning an object with a status of 'success' and a success message
             return {status: 'success', payload: `Product added successfully with id ${product.id}`};
 
         } catch (error) {
             return {status: 'failure', payload: `Product add error: ${error.message}`};
         }
-
     }
 
-    async getProducts() {
-        try {
-            const products = await fs.readFile(this.path, 'utf8');
-            return {status: 'success', payload: JSON.parse(products)};
-        } catch (error) {
-            if (error.code === 'ENOENT') {
-                return {status: '404', payload: []}
-            }
-            return {status: 'failure', payload: error.message}
-        }
-    }
-
+    // Method to get a product by its ID
     async getProductById(id) {
+        // If no ID is provided, returning an object with a status of 'failure' and a message
         if (!id) {
             return {status: 'failure', payload: 'Id is required'};
         }
         try {
+            // Getting all products
             const productsResult = await this.getProducts();
 
+            // If there is an error, returning an object with a status of 'failure' and a message
             if (productsResult.status === 'failure') {
                 return {status: 'failure', payload: 'Error getting products'};
             }
 
+            // Finding the product with the specified ID and returning an object with a status of 'success' and the product
             const products = productsResult.payload;
             const product = products.find(product => product.id === id);
             return product ? {status: 'success', payload: product} : {status: 'failure', payload: 'Product not found'};
@@ -69,34 +88,45 @@ class ProductManager {
         }
     }
 
+    // Define a method to update a product by its id
     async updateById(id, data) {
+        // If no ID is provided, returning an object with a status of 'failure' and a message
         if (!id) {
             return {status: 'failure', payload: 'Id is required'};
         }
 
+        // If the ID is provided in the data, returning an object with a status of 'failure' and a message
         if (data.id) {
             return {status: 'failure', payload: 'Id cannot be updated'};
         }
 
+        // Validating the product's fields using the validateProduct helper function
         const allowedFieldsResult = validateAllowedFields(data, this.requiredProductFields);
 
+        // If validation fails, returning an object with a status of 'failure' and the validation payload
         if (allowedFieldsResult.status === 'failure') {
             return {status: 'failure', payload: allowedFieldsResult.payload};
         }
 
         try {
+            // Getting all products
             const productsResult = await this.getProducts();
 
+            // If getProducts method fails, return failure with an error message
             if (productsResult.status === 'failure') {
                 return {status: 'failure', payload: 'Error getting products'};
             }
 
+            // Finding the product with the specified ID and updating it
             const products = productsResult.payload;
             const index = products.findIndex(product => product.id === id)
+
+            // If the product is not found, returning an object with a status of 'failure' and a message
             if (index === -1) {
                 return {status: 'failure', payload: 'Product not found'};
             }
 
+            // Updating the product and writing to the file
             const product = products[index];
             products[index] = {...product, ...data};
             await fs.writeFile(this.path, JSON.stringify(products));
@@ -108,23 +138,31 @@ class ProductManager {
         }
     }
 
+    // Define a method to delete a product by its id
     async deleteById(id) {
+        // If no ID is provided, returning an object with a status of 'failure' and a message
         if (!id) {
             return {status: 'failure', payload: 'Id is required'}
         }
         try {
+            // Call getProducts method
             const productsResult = await this.getProducts();
 
+            // If getProducts method fails, return failure with an error message
             if (productsResult.status === 'failure') {
                 return {status: 'failure', payload: 'Error getting products'};
             }
 
+            // Find the product with the specified ID and delete it
             const products = productsResult.payload;
             const index = products.findIndex(product => product.id === id)
+
+            // If the product is not found, returning an object with a status of 'failure' and a message
             if (index === -1) {
                 return {status: 'failure', payload: 'Product not found'};
             }
 
+            // Deleting the product and writing to the file
             products.splice(index, 1)
 
             await fs.writeFile(this.path, JSON.stringify(products));
